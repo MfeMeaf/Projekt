731e586f02e9b6e5dbf3d6e7a4d22aeb5b5f0f17
bootleg-cool <richard.larsson03@hotmail.se>
2021-11-18 08:39:40 +0100

    försök för autoplay

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,10 @@
-var rickroll = document.getElementById("rick").autoplay;
+var rickroll = $("#rick")[0];
+rickroll.prop("muted", true);
+
+$(document).ready("load", function(){
+    rickroll.autoplay = true;
+});
+
 var test = document.getElementsByClassName("banner");
 
 test[0].style.backgroundImage = "url('bilder/MfeChu.png')";
