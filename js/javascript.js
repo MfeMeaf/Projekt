a61481a0546db1d4feb7e51246c06ed312c2f122
DeeGCode <83810799+DeeGCode@users.noreply.github.com>
2021-11-18 12:18:36 +0100

    script fix

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,21 @@
+//Banner (skriv efter 2 rader)
+
 var rickroll = document.getElementById("rick").autoplay;
 var test = document.getElementsByClassName("banner");
 
 test[0].style.backgroundImage = "url('bilder/MfeChu.png')";
+
+
+//Teaser (skriv efter 2 rader)
+
+
+var teaserbilder = new Array();
+var vbild = document.getElementById("vänsterbild");
+
+teaserbilder[0]= new Image();
+teaserbilder[0].src = "url('bilder/memes/abgs.png')"
+
+
+//Gallery (skriv efter 2 rader)
+
+
