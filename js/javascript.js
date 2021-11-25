4e55a158fcf70a43ea9d63ae60b27099f7ecd833
MfeMeaf <80445981+MfeMeaf@users.noreply.github.com>
2021-11-25 13:54:42 +0100

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -8,30 +8,31 @@ test[0].style.backgroundImage = "url('bilder/MfeChu.png')";
 
 
 //Teaser (skriv efter 2 rader)
-$("dankmemes-index").on("click", function () {
+$("dankmemes-index").on("click",function(){
     document.location.replace("/category-sidor/dank.html");
 });
 
 document.addEventListener("keydown", piltryck);
-const teaserbilder = ["bilder/memes/1.png", "bilder/memes/2.png", "bilder/memes/3.png", "bilder/memes/4.png", "bilder/memes/5.png", "bilder/memes/6.png", "bilder/memes/7.png", "bilder/memes/8.png", "bilder/memes/9.png", "bilder/memes/10.png", "bilder/memes/11.png", "bilder/memes/12.png", "bilder/memes/13.jpg", "bilder/memes/14.jpg", "bilder/memes/15.jpg", "bilder/memes/16.png", "bilder/memes/17.png", "bilder/memes/18.png"];
-var i = 0;
+const teaserbilder = ["bilder/memes/1.png","bilder/memes/2.png","bilder/memes/3.png","bilder/memes/4.png","bilder/memes/5.png","bilder/memes/6.png","bilder/memes/7.png","bilder/memes/8.png","bilder/memes/9.png","bilder/memes/10.png","bilder/memes/11.png","bilder/memes/12.png","bilder/memes/13.jpg","bilder/memes/14.jpg","bilder/memes/15.jpg","bilder/memes/16.png","bilder/memes/17.png","bilder/memes/18.png"];
+var i=0;
 
-function piltryck(event) {
-    if (event.key === "ArrowRight") {
-        if (i == 16) {
+function piltryck(event){
+    if(event.key === "ArrowRight"){
+        if(i==16){
             i = 0;
-            $("#hbild").attr("src", teaserbilder[i + 1])
+            $("#hbild").attr("src", teaserbilder[i+1])
             $("#mbild").attr("src", teaserbilder[i]);
             $("#vbild").attr("src", teaserbilder[17]);
-        } else {
-            i = i + 1;
+        }
+        
+        else{
+            i = i+1;
             $("#hbild").attr("src", teaserbilder[2 + i])
             $("#mbild").attr("src", teaserbilder[1 + i]);
             $("#vbild").attr("src", teaserbilder[i]);
         }
     }
 
-<<<<<<< HEAD
     if(event.key === "ArrowLeft"){
         if(i==-1){
             i=17;
@@ -42,13 +43,6 @@ function piltryck(event) {
         
         else{
             i = i-1;
-=======
-    if (event.key === "ArrowLeft") {
-        if (i == -1) {
-            return;
-        } else {
-            i = i - 1;
->>>>>>> d39d5d7f1baa953e696d03037db3ed2a21affcab
             $("#hbild").attr("src", teaserbilder[2 + i])
             $("#mbild").attr("src", teaserbilder[1 + i]);
             $("#vbild").attr("src", teaserbilder[i]);
@@ -58,25 +52,40 @@ function piltryck(event) {
 
 var hbtn = document.getElementById("högerknapp");
 var vbtn = document.getElementById("vänsterknapp");
-hbtn.addEventListener("click", högertryck);
-vbtn.addEventListener("click", vänstertryck);
+hbtn.addEventListener("click",högertryck);
+vbtn.addEventListener("click",vänstertryck);
 
-function högertryck() {
+function högertryck(){
 
-    if (i == 16) {
-
-        return;
-    } else {
-        i++;
-        document.getElementById("hbild").src = teaserbilder[2 + i];
-        document.getElementById("mbild").src = teaserbilder[1 + i];
-        document.getElementById("vbild").src = teaserbilder[i];
-    }
+        if(i==16){
+            
+            return;
+        }
+        
+        else{
+            i++;
+            document.getElementById("hbild").src = teaserbilder[2 + i];
+            document.getElementById("mbild").src = teaserbilder[1 + i];
+            document.getElementById("vbild").src = teaserbilder[i];
+        }
 }
 
-function vänstertryck() {
+function vänstertryck(){
 
-<<<<<<< HEAD
+        if(i==-1){
+            
+            return;
+        }
+        
+        else{
+            i--;
+            document.getElementById("hbild").src = teaserbilder[2 + i];
+            document.getElementById("mbild").src = teaserbilder[1 + i];
+            document.getElementById("vbild").src = teaserbilder[i];
+        }
+}
+
+
 function bilddelay(){
     setInterval(autoscroll,5000);
 }
@@ -91,18 +100,16 @@ function autoscroll(){
     
 }
 */
-=======
-    if (i == -1) {
->>>>>>> d39d5d7f1baa953e696d03037db3ed2a21affcab
 
-        return;
-    } else {
-        i--;
-        document.getElementById("hbild").src = teaserbilder[2 + i];
-        document.getElementById("mbild").src = teaserbilder[1 + i];
-        document.getElementById("vbild").src = teaserbilder[i];
+function autoscroll(){
+    console.log(`before: ${i}`)
+    if(i < 16){
+            document.getElementById("hbild").src = teaserbilder[2 + i];
+            document.getElementById("mbild").src = teaserbilder[1 + i];
+            document.getElementById("vbild").src = teaserbilder[i];
+            
+        console.log(i);
     }
-<<<<<<< HEAD
     if(i > 16){
         i=0;
     
@@ -120,312 +127,5 @@ function autoscroll(){
 }
 
 
-=======
-}
 
->>>>>>> d39d5d7f1baa953e696d03037db3ed2a21affcab
-
-
-
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//OWO
-=======
 //Gallery (skriv efter 2 rader)
->>>>>>> d39d5d7f1baa953e696d03037db3ed2a21affcab
